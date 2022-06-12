557046c5864805161924f1206cbcf00cab69a453
hmode1999 <107334072+hmode1999@users.noreply.github.com>
2022-06-12 06:25:22 +0300

    Update version.php

--- version.php
+++ version.php
@@ -1,12 +1,9 @@
 <?php 
 
 
-echo "Version=1.2;";
+echo "Version=1.4;";
 
 /* --------------------------------- ABOUT -------------------------------------
-Original Author: UpLUK
-Website: https://plexuscheat.my.id
-Telegram: https://t.me/bacodluamsu
-Telegram Channel: https://t.me/PLEXUSCH
+تحديث جديد
 ----------------------------------------------------------------------------- */
 ?>
