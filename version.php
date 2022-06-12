4ec49744cd19ec0a2a6d51082dae9ab160f5e1e3
hmode1999 <107334072+hmode1999@users.noreply.github.com>
2022-06-12 06:19:55 +0300

    Add files via upload

---
+++ version.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+echo "Version=1.2;";
+
+/* --------------------------------- ABOUT -------------------------------------
+Original Author: UpLUK
+Website: https://plexuscheat.my.id
+Telegram: https://t.me/bacodluamsu
+Telegram Channel: https://t.me/PLEXUSCH
+----------------------------------------------------------------------------- */
+?>
